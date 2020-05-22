2246f6c104567d1ad39da65ca7be2d67d614d9de
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-05-22 18:06:28 +0200

    Add coveralls.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ REQUIRES_PYTHON = '>=3.5'
 REQUIRED = ['requests', 'bs4', 'lxml']
 EXTRAS = {
     'dev': ['twine'],
-    'test': ['pytest', 'flake8', 'tox']
+    'test': ['pytest', 'pytest-cov', 'coveralls', 'flake8', 'tox']
 }
 
 HERE = os.path.abspath(os.path.dirname(__file__))
